8adf6283d038953bb2f5d41dba9a2b3737ea1a78
Neuze <87799560+Neutralised@users.noreply.github.com>
2025-09-13 20:39:40 +0800

    优化了 encode() 以避免出现参数头过长的问题

--- script/utils/Store.js
+++ script/utils/Store.js
@@ -32,16 +32,20 @@ class Store {
         return data;
     }
 
+    // 修改后的encode方法，仅传递必要的信息，避免URL过长导致431错误
     encode() {
         const data = new Map();
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);
-            if (key.startsWith(Store.prefix)) {
-                data.set(key, localStorage.getItem(key));
+        // 只保留几个必要的参数，而不是所有localStorage数据
+        const necessaryKeys = ['portal-2d-toLoad'];
+        
+        for (const key of necessaryKeys) {
+            const value = localStorage.getItem(key);
+            if (value) {
+                data.set(key, value);
             }
         }
+        
         const param = new URLSearchParams(data);
-
         return param.toString();
     }
 }
